6eb9f93e70d60b2eb6967d6965badb3c3832cf44
Yiqian Yang <yiqianya@usc.edu>
2024-04-11 04:24:36 -0700

    frontend and backend setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+}
+);
+
+app.listen(PORT, () => console.log("Server running on port 4000"));
